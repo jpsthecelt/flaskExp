fcd9f2cc9cc70b006e9034c77a6e98d3d2cc6e09
jpsthecelt <jpsthecelt@gmail.com>
2019-10-09 10:34:38 -0700

    updated pynmnea2ex.py

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -1,14 +1,21 @@
+import sys
 import serial
 import pynmea2
 
-def parseGPS(str):
+def parseGPS(bites):
+    str = bites.decode()
     if str.find('GGA') > 0:
         msg = pynmea2.parse(str)
         print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
               (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
 
-serialPort = serial.Serial("/dev/serial0", 9600, timeout=0.5)
+try:
+    serialPort = serial.Serial("/dev/serial0", 9600, timeout=0.5)
 
-while True:
-    str = serialPort.readline()
-    parseGPS(str)
+    while True:
+        str = serialPort.readline()
+        parseGPS(str)
+
+except (KeyboardInterrupt,SystemExit):
+    print("...Terminated!")
+    sys.exit()
