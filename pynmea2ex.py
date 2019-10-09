9826fa8778c7f5e0717d483ad7174bfcf0dd93a2
jpsthecelt <jpsthecelt@gmail.com>
2019-10-09 08:57:37 -0700

    Updated example program

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -4,11 +4,11 @@ import pynmea2
 def parseGPS(str):
     if str.find('GGA') > 0:
         msg = pynmea2.parse(str)
-        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude:
-        %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,m sg.altitude_units))
+        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
+              (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
 
-    serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
+serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
 
-    while True:
-        str = serialPort.readline()
-        parseGPS(str)
+while True:
+    str = serialPort.readline()
+    parseGPS(str)
