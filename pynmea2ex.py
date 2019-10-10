4140067ac625aa1ebe61fa6e941c3100e8bfbced
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-09 18:22:32 -0700

    updated pynmea2ex to try and use streams

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import pydevd_pycharm
-import serial
-import pynmea2
-
-pydevd_pycharm.settrace('localhost', port=59488, stdoutToServer=True, stderrToServer=True)
-def parseGPS(str):
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
-        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude:
-        %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
-
-serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
-
-while True:
-    str = serialPort.readline()
-    parseGPS(str)
-=======
 import sys
 import serial
 import pynmea2
+
+#def parseGPS(str):
+#    if str.find('GGA') > 0:
+#        msg = pynmea2.parse(str)
+#        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude:
+#        %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
 
 def parseGPS(bites):
     str = bites.decode()
@@ -26,6 +14,12 @@ def parseGPS(bites):
         msg = pynmea2.parse(str)
         print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
               (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
+
+#serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
+#
+#while True:
+#    str = serialPort.readline()
+#    parseGPS(str)
 
 try:
     serialPort = serial.Serial("/dev/serial0", 9600, timeout=0.5)
@@ -37,4 +31,3 @@ try:
 except (KeyboardInterrupt,SystemExit):
     print("...Terminated!")
     sys.exit()
->>>>>>> fcd9f2cc9cc70b006e9034c77a6e98d3d2cc6e09
