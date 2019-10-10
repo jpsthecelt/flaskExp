9e598bbe31a92a0bd82b603170c962a396b1a7b4
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-10 07:20:18 -0700

    modified to use NMEAstream & return JSON

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -2,18 +2,13 @@ import sys
 import serial
 import pynmea2
 
-#def parseGPS(str):
-#    if str.find('GGA') > 0:
-#        msg = pynmea2.parse(str)
-#        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude:
-#        %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
-
-def parseGPS(bites):
-    str = bites.decode()
+def parseGPS(str):
     if str.find('GGA') > 0:
         msg = pynmea2.parse(str)
-        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
-              (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
+#        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
+#              (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
+        return json.dumps(("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
+                  (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)))
 
 #serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
 #
@@ -22,11 +17,10 @@ def parseGPS(bites):
 #    parseGPS(str)
 
 try:
-    serialPort = serial.Serial("/dev/serial0", 9600, timeout=0.5)
-
-    while True:
-        str = serialPort.readline()
-        parseGPS(str)
+        with serial.Serial("/dev/serial0", 9600, timeout=0.5) as sin:
+        while True:
+            str = sin.readline().decode()
+            print(parseGPS(str))
 
 except (KeyboardInterrupt,SystemExit):
     print("...Terminated!")
