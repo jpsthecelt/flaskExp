f83eeb53faf01a5a10cd723ddf63936cea30af20
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-11 14:24:33 -0700

    Removed extraneous printing noise

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -7,11 +7,11 @@ def parseGPS(rawMesg):
     try:
         msg = pynmea2.parse(rawMesg)
     except pynmea2.ParseError:
-        print("Discarded message: {}".format(rawMesg))
-        return str("Parse Error: {}".format(rawMesg))
+#       print("Discarded message: {}".format(rawMesg))
+        return str("{"+"Parse Error: {}".format(rawMesg)+"}")
 
     if msg.sentence_type != 'GGA':
-        print("Discarded message: {}".format(rawMesg))
+        return str("{"+"Message discarded: {}".format(msg.sentence_type)+"}")
     else:
         return str("{"+"Timestamp: {}, Lat: {} {} ,  Lon: {} {}, Altitude: {} {}".format(
                   msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)+"}")
@@ -19,7 +19,7 @@ def parseGPS(rawMesg):
 try:
     with serial.Serial("/dev/serial0", 9600, timeout=0.5) as gIn:
         for i in range(5):
-            print("Initial read; clearing out initial data: %s", gIn.readline().decode('ascii', errors='replace'))
+            print("Initial data synchronization: %s", gIn.readline().decode('ascii', errors='replace'))
             
         while True:
             print("parsed output message is: ", parseGPS(gIn.readline().decode('ascii', errors='replace')))
