a0a467d301c8a39d8f1941c21c7c0c99e971f01f
jpsthecelt <jpsthecelt@gmail.com>
2019-10-11 13:57:09 -0700

    Finally fixed output (but lat & lon not yet working)

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -3,37 +3,26 @@ import serial
 import pynmea2
 import json
 
-def parseGPS(str):
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
-#        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
-#              (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
-        return json.dumps(("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % 
-                  (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)),indent=4)
+def parseGPS(rawMesg):
+    try:
+        msg = pynmea2.parse(rawMesg)
+    except pynmea2.ParseError:
+        print("Discarded message: {}".format(rawMesg))
+        return str("Parse Error: {}".format(rawMesg))
 
-#serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
-#
-#while True:
-#    str = serialPort.readline()
-#    parseGPS(str)
+    if msg.sentence_type != 'GGA':
+        print("Discarded message: {}".format(rawMesg))
+    else:
+        return str("{"+"Timestamp: {}, Lat: {} {} ,  Lon: {} {}, Altitude: {} {}".format(
+                  msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)+"}")
 
 try:
     with serial.Serial("/dev/serial0", 9600, timeout=0.5) as gIn:
         for i in range(5):
-            data = gIn.readline().decode('ascii', errors='replace')
-            print("Initial read; clearing out initial data: {}", data)
+            print("Initial read; clearing out initial data: %s", gIn.readline().decode('ascii', errors='replace'))
             
-        streamReader = pynmea2.NMEAStreamReader()
         while True:
-#           data = gIn.readline().decode('ascii', errors='replace')
-#           newData = pynmea2.parse(data)
-#            print(json.dumps(newData))
-#           data = gIn.readline().decode('ascii', errors='replace')
-            data = gIn.read()
-            for msg in streamReader.next(data):
-                print("message is: {}",msg)
-#            for msg in gIn.next():
-#                print(msg)
+            print("parsed output message is: ", parseGPS(gIn.readline().decode('ascii', errors='replace')))
 
 except (KeyboardInterrupt,SystemExit):
     print("...Terminated!")
