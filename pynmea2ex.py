5573c029b36ad1cbb2161419ee131b524900929b
jpsthecelt <jpsthecelt@gmail.com>
2019-10-11 19:16:16 -0700

    provided print default values, better parse-error text, removed output preamble

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -7,20 +7,20 @@ def parseGPS(rawMesg):
     try:
         msg = pynmea2.parse(rawMesg)
     except pynmea2.ParseError:
-        return str("{{Parse Error: {}}}".format(rawMesg))
+        return str("{{Error: {}}}".format(rawMesg))
 
     if msg.sentence_type != 'GGA':
         return str("{{Message discarded: {}}}".format(msg.sentence_type))
     else:
         return str("{{Timestamp: {}, Lat: {} {} ,  Lon: {} {}, Altitude: {} {}}}".format(
-                  msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
+                  msg.timestamp,msg.lat or 0.0,msg.lat_dir,msg.lon or 0.0,msg.lon_dir,msg.altitude or 0.0,msg.altitude_units or 'M'))
 
 try:
     with serial.Serial("/dev/serial0", 9600, timeout=0.5) as gIn:
-        [print("Initial data synchronization: {}".format(gIn.readline().decode('ascii', errors='replace'))) for i in range(5)]
+        [print("Synchronizing...: {}".format(gIn.readline().decode('ascii', errors='replace'))) for i in range(5)]
             
         while True:
-            print("parsed output message is: ", parseGPS(gIn.readline().decode('ascii', errors='replace')))
+            print(parseGPS(gIn.readline().decode('ascii', errors='replace')))
 
 except (KeyboardInterrupt,SystemExit):
     print("...Terminated!")
