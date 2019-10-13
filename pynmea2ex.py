65cdbaa1fd5d17b2013cd700a51d800dadd44bff
jpsthecelt <jpsthecelt@gmail.com>
2019-10-12 19:57:05 -0700

    fixed formatting

--- pynmea2ex.py
+++ pynmea2ex.py
@@ -7,10 +7,10 @@ def parseGPS(rawMesg):
     try:
         msg = pynmea2.parse(rawMesg)
     except pynmea2.ParseError:
-        return str("{{Error: {}}}".format(rawMesg))
+        return str("{{Parse-Error: {}}}".format(rawMesg))
 
     if msg.sentence_type != 'GGA':
-        return str("{{Message discarded: {}}}".format(msg.sentence_type))
+        return str("{{Discarded: {}}}".format(msg.sentence_type))
     else:
         return str("{{Timestamp: {}, Lat: {} {} ,  Lon: {} {}, Altitude: {} {}}}".format(
                   msg.timestamp,msg.lat or 0.0,msg.lat_dir,msg.lon or 0.0,msg.lon_dir,msg.altitude or 0.0,msg.altitude_units or 'M'))
