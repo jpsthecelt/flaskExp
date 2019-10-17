ecfbcace3ec85556a2495f3e13084862d45d09b3
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-17 07:37:43 -0700

    syncd changes from pi and local windows repositories

---
+++ flask-bgPynmea2ex.py
@@ -0,0 +1,47 @@
+import threading
+import atexit
+from flask import Flask
+
+POOL_TIME = 5 #Seconds
+
+# Called from gunicorn as follows:
+#gunicorn -b 0.0.0.0:5000 --log-config log.conf --pid=app.pid myfile:app
+
+# variables that are accessible from anywhere
+commonDataStruct = {}
+# lock to control access to variable
+dataLock = threading.Lock()
+# thread handler
+yourThread = threading.Thread()
+
+def create_app():
+    app = Flask(__name__)
+
+    def interrupt():
+        global yourThread
+        yourThread.cancel()
+
+    def doStuff():
+        global commonDataStruct
+        global yourThread
+        with dataLock:
+        # Do your stuff with commonDataStruct Here
+
+        # Set the next thread to happen
+        yourThread = threading.Timer(POOL_TIME, doStuff, ())
+        yourThread.start()   
+
+    def doStuffStart():
+        # Do initialisation stuff here
+        global yourThread
+        # Create your thread
+        yourThread = threading.Timer(POOL_TIME, doStuff, ())
+        yourThread.start()
+
+    # Initiate
+    doStuffStart()
+    # When you kill Flask (SIGTERM), clear the trigger for the next thread
+    atexit.register(interrupt)
+    return app
+
+app = create_app()   
--- flask-pynmea2ex.py
+++ flask-pynmea2ex.py
@@ -2,24 +2,32 @@ import sys
 import serial
 import pynmea2
 import json
-from flask import Flask,jsonify
-import pdb
-import time
 
+from flask import flask,jsonify
+
+import time
+from collections import deque
+
+# Given an NMEA input string, parse and either return it's T/S, alt, lat/lon, etc (as a JSON dict) from the GGA message,
+#       or add message-type to the discardQ (returning JSON dict message), or return parse-error message.
 def parseGPS(rawMesg):
+    # try parsing NMEA message - if it fails, return error
+    #                            if it succeeds, look for a message type of 'GGA'
     try:
         msg = pynmea2.parse(rawMesg)
     except pynmea2.ParseError:
-        return str("{{Error: {}}}".format(rawMesg))
+        return (f"{{Parse-Error: {rawMesg}}}")
 
+    # if msg-type is not GGA, add the message to the discarded-Q and return appropriate error-message
+    # if the msg-type IS GGA, return the timestamp/altitude-lat-lon, etc information
     if msg.sentence_type != 'GGA':
-        return str("{{Discarded: {}}}".format(msg.sentence_type))
+        discardQ.append(msg.sentence_type)
+        return (f"{{Discarded: {msg.sentence_type}}}")
     else:
-        print(jsonify(timestamp=msg.timestamp,lat=msg.lat,latDir=msg.lat_dir,
-            lon=msg.lon,LonDir=msg.lon_dir,
-            altitude=msg.altitude,units=msg.altitude_units))
-        return str("{{Timestamp: {}, Lat: {} {} ,  Lon: {} {}, Altitude: {} {}}}".format(
-                  msg.timestamp,msg.lat or 0.0,msg.lat_dir,msg.lon or 0.0,msg.lon_dir,msg.altitude or 0.0,msg.altitude_units or 'M'))
+        return (f"{{Timestamp: {msg.timestamp}, Lat: {msg.lat or 0.0} {msg.lat_dir},  Lon: {msg.lon or 0.0} {msg.lon_dir}, Altitude: {msg.altitude or 0.0} {msg.altitude_units or 'M'}}}")
+
+flask_app= flask(__name__)
+app = create_app()
 
 @app.route('/')
 def mainGPS():
@@ -27,14 +35,20 @@ def mainGPS():
 
 def runner():
     try:
-        pdb.set_trace()
+        # Initialize discardQ list and and serial-inpuyt channel; clearing out any 'framing errors' in receiving-channel 
+        #            (we chose to clear out 5 of them)
+        QMAX = 100
+        discardQ = deque(maxlen=QMAX)
         with serial.Serial("/dev/serial0", 9600, timeout=0.5) as gIn:
             [print("Synchronizing...: {}".format(gIn.readline().decode('ascii', errors='replace'))) for i in range(5)]
                 
+            # Then, while it is possible to read messages from the serial-input
             while True:
                 print(parseGPS(gIn.readline().decode('ascii', errors='replace')))
                 time.sleep(1)
-    
+
     except (KeyboardInterrupt,SystemExit):
         print("...Terminated!")
+        print("Last {len(discardQ)} discarded messages were {discardQ}")
         sys.exit()
+
