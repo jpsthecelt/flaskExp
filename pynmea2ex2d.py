1bb137a8663b802985087455f2c3a6c6cb5fc9e2
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-29 09:55:45 -0700

    added logging to discard-case

--- pynmea2ex2d.py
+++ pynmea2ex2d.py
@@ -59,6 +59,7 @@ def parseGPS(raw_mesg, discardIt):
            em="Discarded: %s" % msg.sentence_type))
         else:
            em="Not-Discarded: %s" % msg.sentence_type))
+        logging.info('parseGPS: returning discarded NMEA message...')
         return ( nmea_msg(timestamp = msg.timestamp, lat=0.0, lat_dir='0', lon=0.0, lon_dir='0', altitude=0.0, altitude_units='M', got_fix=fix,
            num_sats=0, error_msg=em))
 
