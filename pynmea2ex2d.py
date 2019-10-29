4153f7f609f23e241f7969012177c8cc077173e0
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-29 09:41:39 -0700

    fixed nmea_msg() constructor errors (including memory-mgt errors; fixed logic, renamed _current & new_nmea_msg, fixed string-encoding error on parse-failure, etc)

--- pynmea2ex2d.py
+++ pynmea2ex2d.py
@@ -23,7 +23,7 @@ class nmea_msg(dict):
     def __getattr__(self, attr):
         return self[attr]
 
-_current_nmea = nmea_msg(
+_current_nmea_msg = nmea_msg(
      timestamp = datetime.now().timestamp(),
      lat =       0.0,
      lat_dir =   '0',
@@ -46,27 +46,27 @@ def parseGPS(raw_mesg, discardIt):
     try:
         msg = pynmea2.parse(raw_mesg)
     except pynmea2.ParseError:
-        logging.info('Failed to parse NMEA message...')
+        logging.info('parsedGPS: Failed to parse NMEA message...')
         return ( nmea_msg(timestamp = datetime.now().timestamp(), lat=0.0, lat_dir='0', lon=0.0, lon_dir='0', altitude=0.0, altitude_units='M',
-             got_fix=False, num_sats=0, error_msg="Parse-Error: %s" & raw_mesg))
+             got_fix=False, num_sats=0, error_msg="Parse-Error: %s" % raw_mesg))
 
     # if msg-type is not GGA, add the message to the discarded-Q and return appropriate error-message
     # if the msg-type IS GGA, return the timestamp/altitude-lat-lon, etc information
-    fix = msg.gps_qual == 1 
     if msg.sentence_type != 'GGA':
+        fix = msg.gps_qual == 1
         discardQ.append(msg.sentence_type)
         if discardIt:
-                 error_msg="Discarded: %s" % msg.sentence_type))
+           em="Discarded: %s" % msg.sentence_type))
         else:
-                 error_msg="Not-Discarded: %s" % msg.sentence_type))
-        return ( nmea_msg(timestamp = datetime.now().timestamp(), lat=0.0, lat_dir='0', lon=0.0, lon_dir='0', altitude=0.0, altitude_units='M', got_fix=fix, 
-            num_sats=0, error_msg))
+           em="Not-Discarded: %s" % msg.sentence_type))
+        return ( nmea_msg(timestamp = msg.timestamp, lat=0.0, lat_dir='0', lon=0.0, lon_dir='0', altitude=0.0, altitude_units='M', got_fix=fix,
+           num_sats=0, error_msg=em))
 
     else:
         # (recall that gps_qual == 0 is 'no fix', or fix=False, 1 == fix, 2-5 are fix-other)
-        logging.info('\nNew NMEA message before return')
+        logging.info('\nparseGPS: Newly parsed NMEA message before return')
         return (nmea_msg(timestamp=msg.timestamp, lat=(msg.lat or 0.0), lat_dir=msg.lat_dir, lon=(msg.lon or 0.0), lon_dir=msg.lon_dir, 
-                altitude=(msg.altitude or 0.0), altitude_units=(msg.altitude_units or 'M'), got_fix=fix, num_sats=0, error_msg=''))
+           altitude=(msg.altitude or 0.0), altitude_units=(msg.altitude_units or 'M'), got_fix=fix, num_sats=0, error_msg=''))
 
 def update_gps():
     try:
@@ -77,14 +77,14 @@ def update_gps():
             # Then, while it is possible to read messages from the serial-input, read & parse input data
             #       printing out result
             while True:
-                new_nmea_value = parseGPS(gIn.readline().decode('ascii', errors='replace'), results.v)
-                logging.info('got new nmea-message; updating...')
-                print("\nupdate_gps: New NMEA message value looks like:",json.dumps(new_nmea_value))
+                nmea_rxd_msg = parseGPS(gIn.readline().decode('ascii', errors='replace'), results.v)
+                logging.info('update_gps: parsed new nmea-message; updating _current...')
+                print("\nupdate_gps: Newly parsed NMEA message looks like:",json.dumps(nmea_rxd_msg))
                 with db_lock:
-                    _current_nmea = new_nmea_value
-                del new_nmea_value
+                    _current_nmea_msg = nmea_rxd_msg
+                    del nmea_rxd_msg
 
-                print("\nUpdated current NMEA message looks like:",json.dumps(_current_nmea))
+                print("\nUpdated current NMEA message looks like:",json.dumps(_current_nmea_msg))
                 time.sleep(1)
     
     # process any system-exit errors or ^c received, outputting our discardQ contents 'before we go'
@@ -113,7 +113,7 @@ if __name__ == '__main__':
     parser.add_argument('gps_device_string', action='store', default='/dev/serial0', help='device from which we will connect for GPS messages')
     results = parser.parse_args()
     
-    print("\nInitial current NMEA message looks like:",json.dumps(_current_nmea))
+    print("\nInitial current NMEA message looks like:",json.dumps(_current_nmea_msg))
     db_lock = threading.Lock()
     gps_update_thread = threading.Timer(GPS_CYCLE_TIME, update_gps, ())
     atexit.register(interrupt)
@@ -125,4 +125,4 @@ if __name__ == '__main__':
         logging.info('main (after sleep)...')
 
         with db_lock:
-            print("\nin main; dumping _current_nmea)",json.dumps(_current_nmea))
+            print("\nin main; dumping _current_nmea_msg)",json.dumps(_current_nmea_msg))
